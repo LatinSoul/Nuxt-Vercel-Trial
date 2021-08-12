7a482859dc92e4a0abadaa618b4b4b1e99574a42
oliver <oliver.carvajal@gmail.com>
2021-08-12 17:57:59 +0200

    added deps + User.js : a mongoDB data model

--- api/mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const dbURI = 'mongodb+srv://oliver:socrates@cluster0.ixjbh.mongodb.net/djio?retryWrites=true&w=majority'
-const dbConn = mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-    .then((result) => { console.log('Successfully connected to DJIO database') }) // eslint-disable-line no-console
-    .catch((err) => console.log('db conn err:', err)) // eslint-disable-line no-console
-
-module.exports = dbConn;
