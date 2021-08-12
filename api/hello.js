0dec8eb46f37b3f590b5b45c4790d4a355299a80
oliver <oliver.carvajal@gmail.com>
2021-08-12 18:10:21 +0200

    modified hello.js: fetching user id from mongoDB

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,20 @@
-const hello = (req, res) => {
-    res.status('200').json({ message: 'back to export default' });
+const User = require('../models/User')
+
+const hello = async (req, res) => {
+    // res.status('200').json({ message: 'back to export default' });
+    try {
+        const user = await User.findOne({ email: 'oc@gmail.com' })
+        // console.log('uid:', user._id)
+        // const token = createToken(user._id)
+        // res.cookie('user', user, { httpOnly: true })
+        res.status(200).json({ user: user._id })
+    } catch (err) {
+        // const errors = errorHandler(err)
+        res.status(400).json({ errors: err })
+    }
 }
+
+
+
 
 export default hello
