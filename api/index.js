93c2638da9331fa3ab6bcc179c722c68c372f42f
oliver <oliver.carvajal@gmail.com>
2021-08-12 18:31:47 +0200

    restore dbConn js files

---
+++ api/index.js
@@ -0,0 +1,10 @@
+// Import the dependency.
+const dbConn = require('./mongodb');
+// Handler
+module.exports = async (req, res) => {
+    // Get the MongoClient by calling await on the promise.
+    // Because it is a promise, it will only resolve once.
+    const client = await dbConn;
+    // Use the client to return the name of the connected database.
+    res.status(200).json({ dbName: client.db().databaseName });
+}
