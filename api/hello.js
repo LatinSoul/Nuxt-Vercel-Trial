e285595206960213aa41710c287c8591a999f11a
oliver <oliver.carvajal@gmail.com>
2021-08-12 16:54:36 +0200

    changed hello to module.exports

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,8 @@
-const hello = (req, res) => {
-    res.status('200').json({ message: 'Still works!' });
-}
+// const hello = (req, res) => {
+//     res.status('200').json({ message: 'Still works!' });
+// }
 
-export default hello
+// export default hello
+module.exports.hello = (req, res) => {
+    res.status('200').json({ message: 'module.exports.hello' });
+}
