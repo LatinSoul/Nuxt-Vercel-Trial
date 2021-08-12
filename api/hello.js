37c8985d8286ef62d87fbaf1a9703d6614c1c2be
oliver <oliver.carvajal@gmail.com>
2021-08-12 16:56:50 +0200

    back to export default

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,5 @@
-// const hello = (req, res) => {
-//     res.status('200').json({ message: 'Still works!' });
-// }
-
-// export default hello
-module.exports.hello = (req, res) => {
-    res.status('200').json({ message: 'module.exports.hello' });
+const hello = (req, res) => {
+    res.status('200').json({ message: 'back to export default' });
 }
+
+export default hello
