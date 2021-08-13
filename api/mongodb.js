4b55420440645d4114ed6f4dfe5eace626cffec6
oliver <oliver.carvajal@gmail.com>
2021-08-13 18:55:15 +0200

    update mongodb.js with vercel env variable

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 
-const dbURI = 'mongodb+srv://oliver:socrates@cluster0.ixjbh.mongodb.net/djio?retryWrites=true&w=majority'
-const dbConn = mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+// const dbURI = 'mongodb+srv://oliver:socrates@cluster0.ixjbh.mongodb.net/djio?retryWrites=true&w=majority'
+const dbURI = process.env.MONGODB_URI
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}
+const dbConn = mongoose.connect(dbURI, dbOptions)
     .then((result) => { console.log('Successfully connected to DJIO database') }) // eslint-disable-line no-console
     .catch((err) => console.log('db conn err:', err)) // eslint-disable-line no-console
 
