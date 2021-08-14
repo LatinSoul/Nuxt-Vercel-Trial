519765a2baa46b5fb8a18bc54571fae100c716ac
oliver <oliver.carvajal@gmail.com>
2021-08-14 12:12:51 +0200

    using ojbect shorthand 'email' in User.findOne

--- api/hello.js
+++ api/hello.js
@@ -2,8 +2,9 @@ const User = require('./models/User')
 
 const hello = async (req, res) => {
     // res.status('200').json({ message: 'back to export default' });
+    const email = 'oc@gmail.com'
     try {
-        const user = await User.findOne({ email: 'oc@gmail.com' })
+        const user = await User.findOne({ email })
         console.log('uid:', user._id)
         // const token = createToken(user._id)
         // res.cookie('user', user, { httpOnly: true })
