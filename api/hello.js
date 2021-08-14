dd92e241c7df9c57a111578d0a7379355ec5ce87
oliver <oliver.carvajal@gmail.com>
2021-08-14 12:17:23 +0200

    outputting err from User.js

--- api/hello.js
+++ api/hello.js
@@ -5,13 +5,13 @@ const hello = async (req, res) => {
     const email = 'oc@gmail.com'
     try {
         const user = await User.findOne({ email })
-        console.log('uid:', user._id)
+        // console.log('uid:', user._id)
         // const token = createToken(user._id)
         // res.cookie('user', user, { httpOnly: true })
         res.status(200).json({ user: user._id })
     } catch (err) {
         // const errors = errorHandler(err)
-        res.status(400).json({ errors: err })
+        res.status(400).json({ err })
     }
 }
 
