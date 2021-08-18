46d8d55a11b54e2a8bfeda3f95e0078fb6273ea3
oliver <oliver.carvajal@gmail.com>
2021-08-18 16:06:20 +0200

    upate hello.js

--- api/hello.js
+++ api/hello.js
@@ -3,14 +3,14 @@ import dbConnect from './dbConnect'
 
 const hello = async (req, res) => {
     await dbConnect()
-    const { body } = req
+    const bodyEmail = req.body.email
     const email = 'oc@gmail.com'
     try {
         const user = await User.findOne({ email })
         // console.log('uid:', user._id)
         // const token = createToken(user._id)
         // res.cookie('user', user, { httpOnly: true })
-        res.status(200).json({ user: user._id, msg: body })
+        res.status(200).json({ user: user._id, msg: bodyEmail })
     } catch (err) {
         // const errors = errorHandler(err)
         res.status(400).json({ err })
