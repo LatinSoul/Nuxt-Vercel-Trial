30503ff67d77f69c3b38f419e420c7dd1d4a620e
oliver <oliver.carvajal@gmail.com>
2021-08-18 09:31:11 +0200

    modified index.js as a proper export object

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 // // Import the dependency.
 import dbConnect from './dbConnect'
 // Handler
-module.exports.myConn = async (req, res) => {
+const myConn = async (req, res) => {
     // Get the MongoClient by calling await on the promise.
     // Because it is a promise, it will only resolve once.
     const client = await dbConnect();
     // Use the client to return the name of the connected database.
     res.status(200).json({ dbName: client.db().databaseName });
 }
+
+export default myConn
