9212d91cd1866d1a1f9e26ab261b5480d92127cf
oliver <oliver.carvajal@gmail.com>
2021-08-18 14:57:57 +0200

    adding req.body in hello.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,5 @@
 import User from './models/User'
+import errorHandler from './utils/errorHandler'
 import dbConnect from './dbConnect'
 
 const auth = async (req, res) => {
@@ -6,13 +7,12 @@ const auth = async (req, res) => {
     const email = 'oc@gmail.com'
     try {
         const user = await User.findOne({ email })
-        // console.log('uid:', user._id)
         // const token = createToken(user._id)
         // res.cookie('user', user, { httpOnly: true })
         res.status(200).json({ user: user._id })
     } catch (err) {
-        // const errors = errorHandler(err)
-        res.status(400).json({ err })
+        const errors = errorHandler(err)
+        res.status(400).json({ err: errors })
     }
 }
 
--- api/hello.js
+++ api/hello.js
@@ -1,18 +1,16 @@
-// const User = require('./models/User')
-// const dbConnect = require('./dbConnect')
 import User from './models/User'
-import dbConnect from './project'
+import dbConnect from './dbConnect'
 
 const hello = async (req, res) => {
-    // res.status('200').json({ message: 'back to export default' });
     await dbConnect()
+    // const body = req.body
     const email = 'oc@gmail.com'
     try {
         const user = await User.findOne({ email })
         // console.log('uid:', user._id)
         // const token = createToken(user._id)
         // res.cookie('user', user, { httpOnly: true })
-        res.status(200).json({ user: user._id })
+        res.status(200).json({ user: user._id, msg: req.body })
     } catch (err) {
         // const errors = errorHandler(err)
         res.status(400).json({ err })
