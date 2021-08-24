143d6457d1dbe07b23a4746b7a1cb68d2ea01e67
oliver <oliver.carvajal@gmail.com>
2021-08-24 15:12:06 +0200

    adding djio api files

--- api/index.js
+++ api/index.js
@@ -1,16 +1,58 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser')
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+// const { v4 } = require('uuid');
+// Middleware resources
+app.use(cookieParser())
+app.use(express.json())
+app.use('/auth', authRoutes)
+app.use('/user', userRoutes)
 
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
+// app.get('/api', (req, res) => {
+//     const path = `/api/item/${v4()}`;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
+
+// app.get('/api/item/:slug', (req, res) => {
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug}`);
+// });
 
 module.exports = app;
+
+
+// Packages
+// const express = require('express')
+// const cookieParser = require('cookie-parser')
+// const mongoose = require('mongoose')
+// Internal Resources
+// const authRoutes = require('./routes/authRoutes')
+// const userRoutes = require('./routes/userRoutes')
+// const projectRoutes = require('./routes/projectRoutes')
+// const mailerRoutes = require('./routes/mailerRoutes')
+
+// const app = express()
+// MongoDB connexion
+// const dbURI = 'mongodb+srv://oliver:socrates@cluster0.ixjbh.mongodb.net/djio?retryWrites=true&w=majority'
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+    // .then((result) => { console.log('Successfully connected to DJIO database') })
+    // .catch((err) => console.log('db conn err:', err))
+
+// Middleware resources
+// app.use(cookieParser())
+// app.use(express.json())
+// app.use('/auth', authRoutes)
+// app.use('/user', userRoutes)
+// // app.use('/project', projectRoutes)
+// // app.use('/post', postRoutes)
+// app.use('/mailer', mailerRoutes)
+
+// module.exports = {
+//     path: '/api',
+//     handler: app
+// }
