b036bcf6cca4d1b75d7cc0dd8118473b82419253
oliver <oliver.carvajal@gmail.com>
2021-09-16 08:15:32 +0200

    reinit serverless project

--- api/index.js
+++ api/index.js
@@ -1,27 +1,30 @@
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
+
+const { v4 } = require('uuid');
+
 const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes')
 
-// const { v4 } = require('uuid');
+
 // Middleware resources
 app.use(cookieParser())
 app.use(express.json())
 app.use('/auth', authRoutes)
 app.use('/user', userRoutes)
 
-// app.get('/api', (req, res) => {
-//     const path = `/api/item/${v4()}`;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-// app.get('/api/item/:slug', (req, res) => {
-//     const { slug } = req.params;
-//     res.end(`Item: ${slug}`);
-// });
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+});
 
 module.exports = app;
 
