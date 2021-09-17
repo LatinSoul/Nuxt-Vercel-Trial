10da0e974702588c9533bb7319cb116e7f9253ae
oliver <oliver.carvajal@gmail.com>
2021-09-17 13:26:00 +0200

    testing test route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -37,8 +37,8 @@ routes.get('/auth/logout', (req, res) => {
 })
 // Test route
 routes.post('/auth/test', (req, res) => {
-    const { test } = req.body
-    res.status(200).json({ status: test })
+    // const { test } = req.body
+    res.status(200).json({ status: req.body })
 })
 
 module.exports = routes
