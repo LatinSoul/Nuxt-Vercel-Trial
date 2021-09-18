02dc0ca9e8e6835d86fb5292878e6dee1407a6e0
oliver <oliver.carvajal@gmail.com>
2021-09-18 19:20:46 +0200

    using index.js as standalone express

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-console */
-const express = require('express')
-const app = express()
+const app = require('express')()
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
-// const { v4 } = require('uuid');
+const { v4 } = require('uuid');
 
 // MongoDB connexion
 const dbURI = 'mongodb+srv://oliver:socrates@cluster0.ixjbh.mongodb.net/djio?retryWrites=true&w=majority'
@@ -16,15 +15,15 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // Middleware resources
 app.use(cookieParser())
-app.use(express.json())
+// app.use(express.json())
 // app.use('/api', routes)
 
-// app.get('/api', (req, res) => {
-//     const path = `/api/item/${v4()}`;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
 // app.get('/api/item/:slug', (req, res) => {
 //     const { slug } = req.params;
