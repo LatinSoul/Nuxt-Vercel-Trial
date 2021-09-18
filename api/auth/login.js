4d39798f4e99c29a834291b1ce6703c359f340fd
oliver <oliver.carvajal@gmail.com>
2021-09-18 17:14:00 +0200

    added errorHnadler func in login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -7,6 +7,27 @@ const { createToken } = require('../utils/jwt')
 // const errorHandler = require('../utils/errorHandler')
 const User = require('../models/User')
 
+function errorHandler(err) {
+    // console.log(err.message, err.code)
+    const errors = { email: '', password: '' }
+    // validating unique user registration
+    // this check comes first, as if the email is already in use, there is no need to validate it at first place!
+    if (err.code === 11000) {
+        errors.email = 'Email already registered.'
+        return errors
+    }
+    // validating inputs errors 
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    } else {
+        errors.null = `User doesn't exist`
+    }
+    return errors
+}
+
+
 module.exports = async (req, res) => {
     // console.log('triggering auth login function')
     const { email } = req.body
@@ -17,8 +38,8 @@ module.exports = async (req, res) => {
         res.cookie('user', user, { httpOnly: true })
         res.status(200).json({ token })
     } catch (err) {
-        // const errors = errorHandler(err)
-        res.status(400).json({ err })
+        const errors = errorHandler(err)
+        res.status(400).json({ errors })
     }
     // res.status(200).json({ status: email })
 }
