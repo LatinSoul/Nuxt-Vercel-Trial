ee85ba36fa369aad2cf3ff2394854e1150012709
oliver <oliver.carvajal@gmail.com>
2021-09-18 16:41:54 +0200

    testing async in login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -3,23 +3,22 @@
 // };
 
 // Auth required files
-// const jwt = require('jsonwebtoken')
-// const { createToken } = require('../utils/jwt')
-// const errorHandler = require('../utils/errorHandler')
-// const User = require('../models/User')
+const { createToken } = require('../utils/jwt')
+const errorHandler = require('../utils/errorHandler')
+const User = require('../models/User')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     // console.log('triggering auth login function')
     const { email } = req.body
-    // try {
-    //     const user = await User.findOne({ email })
-    //     // console.log('uid:', user._id)
-    //     const token = createToken(user._id)
-    //     res.cookie('user', user, { httpOnly: true })
-    //     res.status(200).json({ token })
-    // } catch (err) {
-    //     const errors = errorHandler(err)
-    //     res.status(400).json({ errors })
-    // }
-    res.status(200).json({ status: email })
+    try {
+        const user = await User.findOne({ email })
+        // console.log('uid:', user._id)
+        const token = createToken(user._id)
+        res.cookie('user', user, { httpOnly: true })
+        res.status(200).json({ token })
+    } catch (err) {
+        const errors = errorHandler(err)
+        res.status(400).json({ errors })
+    }
+    // res.status(200).json({ status: email })
 }
