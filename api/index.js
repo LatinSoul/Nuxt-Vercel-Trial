1763f5cbf1642ebea8b5e23cad40575aef6ee614
oliver <oliver.carvajal@gmail.com>
2021-09-20 22:21:03 +0200

    testing

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.get('/api/item/:slug', (req, res) => {
 
 app.get('/api/test', (req, res) => {
     const { test } = req.body
-    res.end(`${test} from route: /api/test`)
+    res.json(`${test} from route: /api/test`)
 })
 
 module.exports = app;
