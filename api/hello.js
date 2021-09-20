c3b74fcdb64c4897120bec182818528a617751fb
oliver <oliver.carvajal@gmail.com>
2021-09-20 13:40:55 +0200

    added express.urlencoded

--- api/hello.js
+++ api/hello.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 // const bodyParser = require('body-parser');
 app.use(express.json())
+app.use(express.urlencoded())
 
 // It is important that the full path is specified here
 app.post('/api/hello', function (req, res) {
