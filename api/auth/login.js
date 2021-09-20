70a17b0f1db6624abcd705eaad4b0da4d92a30f1
oliver <oliver.carvajal@gmail.com>
2021-09-20 22:58:45 +0200

    added urlencoded in login

--- api/auth/login.js
+++ api/auth/login.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded())
 
 const { createToken } = require('../utils/jwt')
 // const errorHandler = require('../utils/errorHandler')
