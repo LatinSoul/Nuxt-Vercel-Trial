24c5d752588d922486b734b25fe48c04b76e5ce8
oliver <oliver.carvajal@gmail.com>
2021-09-20 14:09:47 +0200

    removing urlencoded

--- api/hello.js
+++ api/hello.js
@@ -1,15 +1,13 @@
 const express = require('express')
 
 const app = express()
-// const bodyParser = require('body-parser');
 app.use(express.json())
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 
 
 // It is important that the full path is specified here
 app.post('/api/hello', function (req, res) {
     const { info } = req.body
-    // console.log(info)
     res
         .status(200)
         .json({ info })
