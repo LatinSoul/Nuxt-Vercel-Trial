544c6f6fa9b4f147df5054020abebbe60ab31076
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-09-29 09:22:51 +0200

    update files

--- api-old/handy.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-    const { body } = await req;
-    return res.send(`Hello ${body.name}, you just parsed the request body!`);
-}
--- api-old1/auth/logout.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res) => {
-    // should we remove user info from the cookie?
-    res.status(200).json({ status: 'OK' })
-}
